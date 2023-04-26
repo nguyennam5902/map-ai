41307a5e92a3f2ef6bda4bace590b43e965f1a32
locpt-0253 <pham.tien.loc@sun-asterisk.com>
2023-04-27 01:22:33 +0700

    config git and map

--- src/map.py
+++ src/map.py
@@ -1,3 +1,89 @@
+from const import *
+from point import Point
+
+
 class Map:
     def __init__(self):
+        self.img = None
+        self.map_points = {}
+        self.map_roads = {}
+        self.current_route = []
+        
+    def show_bg(self):
         pass
+    
+    def show_definedRoad(self):
+        pass
+    
+    def show_definedPoint(self):
+        pass
+    
+    def find_route(self, start_pos, end_pos):
+            
+        process_point = {}
+        for point in self.map_points.values:
+            process_point[str(point)] = ProcessPoint(point)
+            
+        if self.map_points[str(start_pos)] is None:
+            process_point[str(start_pos)] = ProcessPoint(start_pos)
+            
+        if self.map_points[str(end_pos)] is None:
+            process_point[str(end_pos)] = ProcessPoint(end_pos)
+            
+        open_lst = {str(start_pos)} 
+        closed_lst = set()
+        
+        found = False
+        
+        while len(open_lst) > 0:
+            # find node with the least f on the open list -> q
+            minf = min([ process_point[key].f for key in open_lst])
+            q = list(process_point.keys)[list(process_point.values).index(minf)]
+            
+            # pop q from open list
+            open_lst.remove(q)
+            closed_lst.add(q)
+            
+            # generate q's successors and set their parent to q
+            current_point = process_point[q]
+            for road, to_point in current_point.point.adjacents:
+                
+                # if successor is the goal, stop searching and output
+                process_to_point = process_point[str(to_point)]
+                current_f = process_to_point.f
+                if process_to_point.pos == end_pos:
+                    process_to_point.parent = current_point
+                    print("FOUND")
+                    found = True
+                    return
+                
+                elif not str(to_point) in closed_lst:
+                    g_new = current_point.g + road.length
+                    h_new = current_point._calc_dist(end_pos)
+                    f_new = g_new + h_new
+                    
+                    if str(to_point) not in open_lst or current_f > f_new:
+                        open_lst.add(str(to_point))
+                        
+                        process_to_point.f = f_new
+                        process_to_point.g = g_new
+                        process_to_point.h = h_new
+                        process_to_point.parent = current_point
+                        
+        if not found:
+            print("CAN'T FIND ROUTE")
+    
+class ProcessPoint:
+    
+    def __init__(self, point):
+        self.point = point
+        self.pos = point.pos
+        self.f = INFINITY
+        self.g = INFINITY
+        self.h = INFINITY
+        self.parent = None
+        
+    def __str__(self):
+        return f'{self.pos}'
+
+    
