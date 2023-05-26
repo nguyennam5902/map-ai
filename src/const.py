572459713e6895200a344a393398cf48eb4b1892
MatsuoRotsuki <loclienhadonganh@gmail.com>
2023-05-26 23:51:18 +0700

    fix README and add function to see closed and open set

--- src/const.py
+++ src/const.py
@@ -14,7 +14,9 @@ INFINITY = 999999
 COLOR = {
     "RED": (255, 0, 0),
     "BLACK": (0, 0, 0),
-    "WHITE": (255, 255, 255)
+    "WHITE": (255, 255, 255),
+    "BLUE": (0, 0, 255),
+    "GREEN": (0, 255, 0)
 }
 '''Dictionary for colors using in the application '''
 
